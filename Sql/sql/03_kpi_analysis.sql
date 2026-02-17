12f28e96347b5c68445d682c01f79041d7325a47
Kennymagic22 <104883128+Kennymagic22@users.noreply.github.com>
2026-02-16 19:33:16 -0500

    Create 03_kpi_analysis.sql

---
+++ Sql/sql/03_kpi_analysis.sql
@@ -0,0 +1,45 @@
+-- Airline Performance Analysis
+-- Author: Kehinde Goriola
+-- Tool: MySQL
+-- File: 03_kpi_analysis.sql
+-- Description: KPI and performance analysis
+
+
+-- Overall Delay Rate
+SELECT 
+    COUNT(*) AS Total_Flights,
+    SUM(CASE WHEN `Flight Status` = 'Delayed' THEN 1 ELSE 0 END) AS Total_Delays,
+    ROUND(100 *
+        SUM(CASE WHEN `Flight Status` = 'Delayed' THEN 1 ELSE 0 END)
+        / COUNT(*), 2) AS Delay_Rate_Percentage
+FROM Cleaned_Flights;
+
+-- Delay Rate by Airport
+SELECT `Airport Name`,
+       COUNT(*) AS Total_Flights,
+       SUM(CASE WHEN `Flight Status` = 'Delayed' THEN 1 ELSE 0 END) AS Delays,
+       ROUND(100 *
+            SUM(CASE WHEN `Flight Status` = 'Delayed' THEN 1 ELSE 0 END)
+            / COUNT(*), 2) AS Delay_Rate
+FROM Cleaned_Flights
+GROUP BY `Airport Name`
+ORDER BY Delay_Rate DESC;
+
+-- Delay Rate by Pilot
+SELECT `Pilot Name`,
+       COUNT(*) AS Total_Flights,
+       SUM(CASE WHEN `Flight Status` = 'Delayed' THEN 1 ELSE 0 END) AS Delays,
+       ROUND(100 *
+            SUM(CASE WHEN `Flight Status` = 'Delayed' THEN 1 ELSE 0 END)
+            / COUNT(*), 2) AS Delay_Rate
+FROM Cleaned_Flights
+GROUP BY `Pilot Name`
+ORDER BY Delay_Rate ASC;
+
+-- Top 10 Airports by Volume
+SELECT `Airport Name`,
+       COUNT(*) AS Total_Flights
+FROM Cleaned_Flights
+GROUP BY `Airport Name`
+ORDER BY Total_Flights DESC
+LIMIT 10;
