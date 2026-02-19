c21384f44f31e74eff3e8878d08775c1dadab3f3
Kennymagic22 <104883128+Kennymagic22@users.noreply.github.com>
2026-02-19 00:54:05 -0500

    Update 03_kpi_analysis.sql

--- Sql/sql/03_kpi_analysis.sql
+++ Sql/sql/03_kpi_analysis.sql
@@ -14,6 +14,7 @@ SELECT
         / COUNT(*), 2) AS Delay_Rate_Percentage
 FROM Cleaned_Flights;
 
+
 -- Delay Rate by Airport
 SELECT `Airport Name`,
        COUNT(*) AS Total_Flights,
@@ -25,6 +26,7 @@ FROM Cleaned_Flights
 GROUP BY `Airport Name`
 ORDER BY Delay_Rate DESC;
 
+
 -- Delay Rate by Pilot
 SELECT `Pilot Name`,
        COUNT(*) AS Total_Flights,
@@ -35,6 +37,7 @@ SELECT `Pilot Name`,
 FROM Cleaned_Flights
 GROUP BY `Pilot Name`
 ORDER BY Delay_Rate ASC;
+
 
 -- Top 10 Airports by Volume
 SELECT `Airport Name`,
